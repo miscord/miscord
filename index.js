df2cd26a1def041be7dd0c04ce3dcc1680936ba6
Bjornskjald <github@bjorn.ml>
2018-01-19 12:32:41 +0100

    added handling in case of login approval

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const Discord = require('discord.js')
 const Messenger = require('facebook-chat-api')
 const fs = require('fs')
 const util = ('util')
+const readline = require('readline')
 const sendError = require('./lib/error.js')
 const removeAccents = require('remove-accents')
+
+var rl = readline.createInterface({input: process.stdin, output: process.stdout})
 
 const discord = new Discord.Client()
 discord.login(process.env.DISCORD_TOKEN).then(u => {
@@ -13,8 +16,15 @@ discord.login(process.env.DISCORD_TOKEN).then(u => {
 })
 
 Messenger({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err,api) => {
-	if(err) return console.error(err)
-    api.setOptions({ logLevel: "silent" })
+	if(err) {
+		if (err.error !== 'login-approval') return console.error(err)
+		console.log('Enter code: ')
+		rl.on('line', line => {
+			err.continue(line)
+			rl.close()
+		})
+	}
+  api.setOptions({ logLevel: "silent" })
 	discord.on("message", message => {
 		if (message.author.username === discord.user.username) return
 		var msg = message.attachments.size > 0 ? {body: message.content, url: message.attachments.first().url} : {body: message.content}
@@ -56,6 +66,4 @@ function createMessage (thread, sender, message) {
 		if (attach[0].type === 'photo') return embed.setImage(attach[0].url)
 		return embed.attachFile(attach.url)
 	}
-	console.log(message)
-	return 'wyjebalo'
 }
