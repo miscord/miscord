1fc97420f355e1297c1c4acaa26db28ad0af836d
Bjornskjald <github@bjorn.ml>
2018-04-01 15:36:33 +0200

    rewritten index for npm global module

---
+++ bin/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+require('../lib/logger.js')
+require('colors')
+
+const sendError = require('../lib/error.js')
+const login = require('../lib/login/login.js')
+
+const discordListener = require('../lib/listeners/discord.js')
+const messengerListener = require('../lib/listeners/messenger.js')
+const getConfig = require('../lib/config/getConfig.js')
+
+if (['--help', '-h'].includes(process.argv[2])) {
+  return console.log(
+    'Miscord v' + require('../package.json').version,
+    '\nUsage:',
+    '\n\tmiscord', '[--help, -h]'.green, 'configPath'.blue
+  )
+}
+
+login(getConfig(process.argv[2])).then(config => {
+  // when got a discord message
+  config.discord.client.on('message', message => discordListener({config, message}))
+
+  // when got a messenger message
+  config.messenger.client.listen((err, message) => messengerListener({config, err, message}))
+}).catch(sendError)
