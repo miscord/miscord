e4ce1540d434c1ab85ee9ef42b1a293a97e270b5
Bjornskjald <github@bjorn.ml>
2018-04-19 17:38:41 +0200

    :bug: fixed printing the warning message on Linux

---
+++ bin/postinstall.js
@@ -0,0 +1,4 @@
+require('colors')
+console.log('Ignore any ' + 'WARN'.black.bgYellow + ' errors below, these are optional dependencies and they\'re not necessary to run Miscord correctly.')
+console.log('Join our Discord server: ' + 'https://discord.gg/DkmTvVz'.blue)
+console.log('') // empty line to separate from NPM warnings
