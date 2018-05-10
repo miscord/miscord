20ebe7beee776c40e2086714176e80f727128b3d
Bjornskjald <github@bjorn.ml>
2018-05-10 17:11:49 +0200

    :art: simplified runscripts

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 require('colors')
 console.log('Ignore any ' + 'WARN'.black.bgYellow + ' errors below, these are optional dependencies and they\'re not necessary to run Miscord correctly.')
 console.log('Join our Discord server: ' + 'https://discord.gg/DkmTvVz'.blue)
