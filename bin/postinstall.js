38a11e5f88528b0d6a0b564202004ca13df9d8fe
Bjornskjald <github@bjorn.ml>
2018-05-10 17:57:16 +0200

    :ambulance: (:green_heart:) fixed "permissions denied" on install

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 require('colors')
 console.log('Ignore any ' + 'WARN'.black.bgYellow + ' errors below, these are optional dependencies and they\'re not necessary to run Miscord correctly.')
 console.log('Join our Discord server: ' + 'https://discord.gg/DkmTvVz'.blue)
