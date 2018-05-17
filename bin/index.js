a3eb626a2124543539c1b8d7ad1de95200ef7b02
Bjornskjald <github@bjorn.ml>
2018-05-17 22:00:37 +0200

    :loud_sound: added more logging

--- bin/index.js
+++ bin/index.js
@@ -42,4 +42,4 @@ getConfig(args.c || args.config).then(login).then(config => {
 
   // when got a messenger message
   config.messenger.stopListening = config.messenger.client.listen((err, message) => messengerListener({config, err, message}))
-}).catch(sendError)
+}).catch(err => sendError('bin', err))
