6837ecf2ac3e7a8f5c978cb750c6354fbdcd7856
Bjornskjald <github@bjorn.ml>
2018-05-31 23:01:53 +0200

    :bug: fixed os recognition

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const download = document.querySelector('#download')
 var osMatch = navigator.platform.match(/(Win|Mac|Linux)/)
 var os = (osMatch && osMatch[1]) || ''
 var arch = navigator.userAgent.match(/x86_64|Win64|WOW64/) || navigator.cpuClass === 'x64' ? 'x64' : 'x86'
@@ -5,13 +6,14 @@ var arch = navigator.userAgent.match(/x86_64|Win64|WOW64/) || navigator.cpuClass
 fetch('https://api.github.com/repos/Bjornskjald/miscord/releases/latest')
   .then(res => res.json())
   .then(res => res.assets
-    .map(el => ({ url: el.browser_download_url, name: el.name.split('-') }))
-    .map(el => ({ url: el.url, version: el.name[1], os: el.name[2], arch: el.name[3].includes('.') ? el.name[3].split('.')[0] : el.name[3] }))
-    .find(el => (el.os === getFullOSName(os).toLowerCase() || el.os === os.toLowerCase()) && el.arch === arch)
+    .map(el => ({ url: el.browser_download_url, name: el.name.split('.').slice(0, 3).join('.').split('-') }))
+    .map(el => ({ url: el.url, version: el.name[1], os: el.name[2], arch: os === 'Mac' ? 'x64' : el.name[3] }))
+    .find(el => (el.os === getFullOSName(os).toLowerCase() || el.os === os.toLowerCase()) && os === 'Mac' ? true : el.arch === arch)
   )
   .then(asset => {
-    document.querySelector('#download').href = asset.url
-    document.querySelector('#download').innerHTML = `Download Miscord ${asset.version} for ${getFullOSName(os)}${os !== 'Mac' ? ` (${asset.arch === 'x64' ? '64-bit' : '32-bit'})` : ``}`
+    download.href = asset.url
+    download.innerHTML = `Download Miscord ${asset.version} for ${getFullOSName(os)}${getArchName(os, asset.arch)}`
   })
 
-function getFullOSName (os) { switch (os) { case 'Win': return 'Windows'; case 'Mac': return 'macOS'; default: return 'Linux' } }
+function getArchName (os, arch) { return os === 'Mac' ? '' : arch === 'x64' ? ' (64-bit)' : ' (32-bit)' }
+function getFullOSName (os) { return {Win: 'Windows', Mac: 'macOS'}[os] || 'Linux' }
