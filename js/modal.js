c7270319fb98d36d1722bee8c2b415b11c3ac4cd
Bjornskjald <github@bjorn.ml>
2018-06-22 23:43:39 +0200

    :art: fixed indentation

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,9 @@
-function toggleModal(element) {
-    if (typeof element === 'string') document.querySelector(element).classList.toggle('is-active')
-    else element.classList.toggle('is-active')
+function toggleModal (element) {
+  if (typeof element === 'string') document.querySelector(element).classList.toggle('is-active')
+  else element.classList.toggle('is-active')
 }
 
 document.querySelectorAll('.modal').forEach(modal => {
-    modal.querySelector('.modal-background').addEventListener(ev => toggleModal(modal))
-    modal.querySelector('.modal-close').addEventListener(ev => toggleModal(modal))
+  modal.querySelector('.modal-background').addEventListener('click', ev => toggleModal(modal))
+  modal.querySelector('.modal-close').addEventListener('click', ev => toggleModal(modal))
 })
