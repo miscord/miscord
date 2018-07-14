7aed218dede3857eb7740701c7e4bcba3cfa8375
Bjornskjald <github@bjorn.ml>
2018-07-14 14:17:34 +0200

    :bug: linting fixes

--- bin/index.js
+++ bin/index.js
@@ -3,21 +3,15 @@ require('colors')
 const printAndExit = m => process.exit(console.log(m) || 0)
 const miscord = require('../')
 const sendError = require('../lib/error')
-const login = require('../lib/login/login')
-
-const discordListener = require('../lib/listeners/discord')
-const messengerListener = require('../lib/listeners/messenger')
-const getConfig = require('../lib/config/getConfig')
-const getConfigDir = require('../lib/config/getConfigDir')
+const { getConfig, getConfigDir } = require('../lib/config')
+const { checkToken } = require('../lib/discord')
 
 var args = require('minimist')(process.argv.slice(2))
 if (args.h || args.help) printAndExit(require('./help'))
 if (args.v || args.version) printAndExit(require('../package.json').version)
 if (args.getConfigPath) printAndExit(require('path').join(getConfigDir(), 'config.json'))
 
-
 require('../lib/logger.js')(args.c || args.config)
-
 
 getConfig(args.c || args.config).then(miscord).catch(err => sendError(err))
 
