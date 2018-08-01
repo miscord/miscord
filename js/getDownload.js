ab78d3b4827f3d6dea663ac3866a1093dc67c342
Bjornskjald <github@bjorn.ml>
2018-08-01 09:48:36 +0200

    :bug: fixed os detection due to new asset naming

--- js/getDownload.js
+++ js/getDownload.js
@@ -1,3 +1,4 @@
+const getFullOSName = os => ({Win: 'Windows', Mac: 'macOS'}[os]) || 'Linux'
 const download = document.querySelector('#download')
 var osMatch = navigator.platform.match(/(Win|Mac|Linux)/)
 var os = (osMatch && osMatch[1]) || ''
@@ -5,15 +6,12 @@ var arch = navigator.userAgent.match(/x86_64|Win64|WOW64/) || navigator.cpuClass
 
 fetch('https://api.github.com/repos/Bjornskjald/miscord/releases/latest')
   .then(res => res.json())
-  .then(res => res.assets
-    .map(el => ({ url: el.browser_download_url, name: el.name.split('.').slice(0, 3).join('.').split('-') }))
-    .map(el => ({ url: el.url, version: el.name[1], os: el.name[2], arch: os === 'Mac' ? 'x64' : el.name[3] }))
-    .find(el => (el.os === getFullOSName(os).toLowerCase() || el.os === os.toLowerCase()) && (os === 'Mac' ? true : el.arch === arch))
+  .then(res => (os === 'Mac' || arch === 'x64') ? res : { assets: [] })
+  .then(release => release.assets
+    .map(el => ({ url: el.browser_download_url, os: el.name.split('.')[0].replace('miscord-', ''), version: release.name }))
+    .find(el => (el.os === getFullOSName(os).toLowerCase() || el.os === os.toLowerCase()))
   )
   .then(asset => {
     download.href = asset.url
-    download.innerHTML = `Download Miscord ${asset.version} for ${getFullOSName(os)}${getArchName(os, asset.arch)}`
+    download.innerHTML = `Download Miscord ${asset.version} for ${getFullOSName(os)}`
   })
-
-function getArchName (os, arch) { return os === 'Mac' ? '' : arch === 'x64' ? ' (64-bit)' : ' (32-bit)' }
-function getFullOSName (os) { return {Win: 'Windows', Mac: 'macOS'}[os] || 'Linux' }
