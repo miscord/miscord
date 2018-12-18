ad02f248108d3514dd348da1eaad44d9cdbe77d0
Bjornskjald <github@bjorn.ml>
2018-12-18 20:09:33 +0100

    :art: cleaned up postInstall script using tagged chalk

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
-console.log('Ignore any ' + chalk.bgYellow.black('WARN') + ' errors below, these are optional dependencies and they\'re not necessary to run Miscord correctly.')
-console.log('Join our Discord server: ' + chalk.blue('https://discord.gg/DkmTvVz'))
-console.log('') // empty line to separate from NPM warnings
+console.log(chalk`
+Ignore any {bgYellow.black WARN} errors below/above, these are optional dependencies and they're not necessary to run Miscord correctly.
+By the way, join our Discord server: {blue.underline https://discord.gg/DkmTvVz}
+`) // empty line to separate from NPM warnings
