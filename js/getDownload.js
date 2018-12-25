64c267c8d2f00c43df9e8da85c9e1aa7e60c4cbe
Bjornskjald <github@bjorn.ml>
2018-12-25 23:50:19 +0100

    :zap: changed api link to avoid redirects

--- js/getDownload.js
+++ js/getDownload.js
@@ -6,7 +6,7 @@ const arch = family !== 'OS X' ? platform.os.architecture : 64
 const os = family.startsWith('Windows') ? (arch === 64 ? 'win' : 'win32') : family === 'OS X' ? 'macapp' : arch === 64 ? 'linux' : 'linux32'
 const longOS = family.startsWith('Windows') ? 'Windows' : family === 'OS X' ? 'macOS' : 'Linux'
 
-fetch('https://api.github.com/repos/Bjornskjald/miscord/releases/latest')
+fetch('https://api.github.com/repositories/111148687/releases/latest')
   .then(res => res.json())
   .then(release => release.assets
     .map(el => ({ url: el.browser_download_url, os: el.name.split('-')[2].split('.')[0], version: release.name }))
