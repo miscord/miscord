cfc112103b86cabd2e486aeb71a84a333606d363
Bjornskjald <github@bjorn.ml>
2019-05-11 10:55:57 +0200

    :sparkles: added splitString util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,6 @@ export function checkIgnoredSequences (m: string) {
   return Array.isArray(config.ignoredSequences) && config.ignoredSequences.map(r => new RegExp(r)).some(r => r.test(m))
 }
 export function truncatePeople (people: string[]) { return people.length > 10 ? people.slice(0, 10).join(', ') + ` and ${people.length - 10} more...` : people.join(', ') }
+export function splitString (str: string, fragmentLength: number): string[] {
+  return str.length > fragmentLength ? str.match(new RegExp(`[\\s\\S]{1,${fragmentLength}}`, 'g'))!! : [ str ]
+}
