01291c5b2bcf1144cb84aedb6903fa514074f979
Bjornskjald <github@bjorn.ml>
2019-05-31 17:43:54 +0200

    :art: removed duplicate code in Messenger mentions

--- src/messenger/handleMentions.ts
+++ src/messenger/handleMentions.ts
@@ -5,11 +5,18 @@ import { Mention } from 'libfb'
 
 export default function handleMentions (message: string, thread: Thread): Mention[] {
   const mentions: Mention[] = []
+  const find = findMentions(message, mentions)
   for (let user of thread.participants) {
-    const offset = message.toLowerCase().indexOf(user.name.toLowerCase())
-    if (offset === -1) continue
-    mentions.push({ offset, length: user.name.length, id: user.id })
+    find(`@${user.name}`, user.id)
+    find(`@${user.name.split(' ')[0]}`, user.id)
+    if (thread.nicknames && thread.nicknames[user.id]) find(`@${thread.nicknames[user.id]}`, user.id)
   }
   log.trace('mentions', mentions)
   return mentions
+}
+function findMentions (message: string, mentions: Mention[]) {
+  return (searchString: string, id: string) => {
+    const offset = message.toLowerCase().indexOf(searchString.toLowerCase())
+    if (offset !== -1) mentions.push({ offset, length: searchString.length, id })
+  }
 }
