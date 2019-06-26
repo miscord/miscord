b9149f979f9eff5ee3119ce6cd81ec6008d39dcc
Bjornskjald <github@bjorn.ml>
2019-06-26 20:48:35 +0200

    :construction: improved discord and messenger API endpoints

--- src/api/discord.ts
+++ src/api/discord.ts
@@ -1,10 +1,25 @@
 import { Server } from '../types/fastify'
+import { Guild, GuildChannel } from 'discord.js'
+
+function channel ({ type, name, id, parent }: GuildChannel) {
+  return {
+    type, name, id,
+    category: parent ? parent.id : null
+  }
+}
+
+function guild ({ name, id, channels }: Guild) {
+  return {
+    name, id,
+    channels: channels.array().map(channel)
+  }
+}
 
 export default async (app: Server) => {
   app.get('/guilds', async (request, reply) => {
-    return discord.client.guilds.array()
+    return discord.client.guilds.array().map(guild)
   })
   app.get('/guilds/:guild/channels', async (request, reply) => {
-    return discord.client.guilds.get(request.params.guild)!!.channels.array()
+    return discord.client.guilds.get(request.params.guild)!!.channels.array().map(channel)
   })
 }
--- src/api/messenger.ts
+++ src/api/messenger.ts
@@ -1,7 +1,12 @@
 import { Server } from '../types/fastify'
+import Thread from '../types/Thread'
+
+function thread ({ name, id, isGroup }: Thread) {
+  return { name, id, isGroup }
+}
 
 export default async (app: Server) => {
   app.get('/threads', async (request, reply) => {
-    return messenger.threads
+    return Array.from(messenger.threads.values()).map(thread)
   })
 }
