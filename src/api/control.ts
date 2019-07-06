62e01c99712b838e032ac99742af2d543a200f66
Bjornskjald <github@bjorn.ml>
2019-07-06 23:29:01 +0200

    :sparkles: added /control/logs to the API

--- src/api/control.ts
+++ src/api/control.ts
@@ -1,27 +1,48 @@
 import { Server } from '../types/fastify'
+import fs from 'fs-extra'
+import FileConfig from '../config/FileConfig'
+import path from 'path'
 
 export default async (app: Server) => {
+  app.get('/logs', async (request, reply) => {
+    if (!(config instanceof FileConfig)) return reply.send([])
+    const count = request.query.count || 100
+
+    const logsFolderPath = path.join(path.parse(config.path).dir, 'logs')
+    const logs = await fs.readdir(logsFolderPath)
+    const file = await fs.readFile(path.join(logsFolderPath, logs.slice(-1)[0]), 'utf8')
+    const lines = file.split('\n')
+
+    return lines.slice(-count)
+  })
+
   app.get('/status', (request, reply) => {
     reply.send({ status: config.paused ? 'paused' : 'running' })
   })
+
   app.post('/pause', (request, reply) => {
     config.paused = true
     reply.send({ status: 'paused' })
   })
+
   app.post('/unpause', (request, reply) => {
     config.paused = false
     reply.send({ status: 'running' })
   })
+
   app.post('/restart', async (request, reply) => {
     reply.send({})
-    await discord.client.destroy()
-    logger.success('Logged out from Discord')
-    console.error('close 2')
+    await close(2)
   })
+
   app.post('/quit', async (request, reply) => {
     reply.send({})
-    await discord.client.destroy()
-    logger.success('Logged out from Discord')
-    console.error('close 0')
+    await close(0)
   })
+}
+
+async function close (code: number) {
+  await discord.client.destroy()
+  logger.success('Logged out from Discord')
+  console.error('close ' + code.toString())
 }
