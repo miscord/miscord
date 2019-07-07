81e3ec987f0768864efbce0976a11617fa796edc
Bjornskjald <github@bjorn.ml>
2019-07-07 20:43:19 +0200

    :construction: added guild and category selects

--- static/js/initSelects.js
+++ static/js/initSelects.js
@@ -1,5 +1,8 @@
 export default function initSelects ({ guilds, threads }) {
-  if (guilds) initChannels(guilds)
+  if (guilds) {
+    initChannels(guilds)
+    initGuilds(guilds)
+  }
   if (threads) initThreads(threads)
 }
 
@@ -25,5 +28,27 @@ export function initChannels (guilds) {
           : `#${channel.name}`
       }))
     }))
+  })
+}
+
+export function initGuilds (guilds) {
+  $('.discord-guild-select').select2({
+    placeholder: 'Select a Discord server',
+    data: guilds.map(guild => ({
+      id: guild.id,
+      text: guild.name
+    }))
+  })
+}
+
+export function initCategories (guild) {
+  $('.discord-category-select').select2({
+    placeholder: 'Select a category',
+    data: guild.channels
+      .filter(channel => channel.type === 'category')
+      .map(category => ({
+        id: category.id,
+        text: category.name
+      }))
   })
 }
