6443b6aee519dc084f314d325ff0cd10e04eeab8
Bjornskjald <github@bjorn.ml>
2019-08-03 22:30:11 +0200

    :speech_balloon: changed "login to" to "log into"

--- src/error/getErrorDescription.ts
+++ src/error/getErrorDescription.ts
@@ -1,15 +1,15 @@
 const errorDescriptions = {
   'Invalid username or email address': `
-Couldn't login to Facebook.
+Couldn't log into Facebook.
 Check your username/email address, it may be incorrect.
 `,
   'Invalid username or password': `
-Couldn't login to Facebook.
+Couldn't log into Facebook.
 Check your password or preferrably, use an app password:
 http://facebook.com/settings?tab=security&section=per_app_passwords&view
 `,
   'Incorrect login details were provided.': `
-Couldn't login to Discord.
+Couldn't log into Discord.
 Check your token.
 (it shouldn't be client ID nor anything else that doesn't have "token" in its name)
 `,
