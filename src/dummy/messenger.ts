0e3cd342296dac754006a40bfbe1067509ff5e24
Bjornskjald <github@bjorn.ml>
2019-08-05 17:22:38 +0200

    :bug: updated mocks to be 100% compatible with the library

--- src/dummy/messenger.ts
+++ src/dummy/messenger.ts
@@ -16,8 +16,37 @@ export default class FakeClient {
   async getAttachmentURL (mid: string, aid: string): Promise<string> { return '' }
   async getStickerURL (stickerID: number): Promise<string> { return '' }
   async getThreadList (count: number): Promise<Thread[]> { return [] }
-  async getThreadInfo (threadID: string): Promise<Thread> { return { isGroup: true, id: threadID } as Thread }
-  async getUserInfo (userID: string): Promise<User> { return { id: userID } as User }
+  async getThreadInfo (threadID: string): Promise<Thread> {
+    return {
+      isGroup: true,
+      id: threadID,
+      name: threadID,
+      participants: [],
+      image: '',
+      unreadCount: 0,
+      canReply: true,
+      cannotReplyReason: '',
+      isArchived: false,
+      color: '#000000',
+      emoji: '',
+      nicknames: null
+    }
+  }
+  async getUserInfo (userID: string): Promise<User> {
+    return {
+      id: userID,
+      name: 'Firstname Lastname',
+      type: 'User',
+      canMessage: true,
+      emailAddresses: undefined,
+      isBlocked: false,
+      isMessengerUser: true,
+      isPage: false,
+      profilePicLarge: '',
+      profilePicMedium: '',
+      profilePicSmall: ''
+    }
+  }
   on (event: string, callback: any) {}
 
   constructor (data?: any) {}
